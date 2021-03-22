253fb423b4ed89ef0a0fa88596ce52a71c8a7bbb
Dikgetho Phala <dikgethop@gmail.com>
2021-03-22 14:21:00 +0200

    Added Newtonsoft.Json package and some models

---
+++ SovChuck/Models/JokeReult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SovChuck.Models
+{
+	public class JokesResult
+	{
+		public string Total { get; set; }
+		public List<Joke> result { get; set; }
+	}
+}
---
+++ SovChuck/Models/RootObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SovChuck.Models
+{
+	public class RootObject
+	{
+		public int count { get; set; }
+		public string next { get; set; }
+		public object previous { get; set; }
+
+		[JsonProperty("results")]
+		public List<People> Peoples { get; set; }
+	}
+}
